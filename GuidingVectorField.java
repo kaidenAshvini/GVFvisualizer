c0912f4c505e03518df23c6c781252eae03ddce0
kaidenAshvini <kaidenashvini@gmail.com>
2024-08-08 17:33:09 +0000

    final ready to copy over to fTc

--- GuidingVectorField.java
+++ GuidingVectorField.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 import java.util.*;
 public class GuidingVectorField {
     double step = 0.01;
-    double distscale = 0.5;
+    double distscale = 0.25;
+    double scale = 20;
     double t;
     public Double[] getClosestValueOfT(Path p, Point location, double estimaT) {
         double lastDist = location.getDist(p.update(estimaT));
@@ -25,7 +26,7 @@ public class GuidingVectorField {
     public Point getVecParallel(Path p, Point loc) {
         Double[] tangent = getClosestValueOfT(p, loc, 0.0);
         double lastHeading = tangent[2];
-        Point popin = new Point(30*Math.sqrt(2)*Math.cos(lastHeading), 30*Math.sqrt(2)*Math.sin(lastHeading));
+        Point popin = new Point(scale*Math.sqrt(2)*Math.cos(lastHeading), scale*Math.sqrt(2)*Math.sin(lastHeading));
         popin.x += loc.x;
         popin.y += loc.y;
         return popin;
@@ -34,19 +35,26 @@ public class GuidingVectorField {
     public Point getVec(Path p, Point loc) {
         Double[] tangent = getClosestValueOfT(p, loc, 0.0);
         double lastHeading = tangent[2];
-        double correction = tangent[1];
-        if (correction > 30) {
-            correction = 30;
+        double correction = tangent[1] *distscale;
+        if (correction > scale) {
+            correction = scale;
         }
 
-        double angleForCos = Math.asin(correction/(30*Math.sqrt(2)));
+        double angleForCos = Math.asin(correction/(scale*Math.sqrt(2)));
         Point popin = new Point(loc.x,loc.y);
 
-        double powerFinal = Math.cos(angleForCos) * (30*Math.sqrt(2));
+        double powerFinal = (scale*Math.sqrt(2));
+//        Math.cos(angleForCos) *
+        double rotAngle = -angleForCos-angleForCos;
+//        double xcomp = powerFinal*Math.cos(lastHeading+angleForCos+rotAngle);
+//        double ycomp = powerFinal*Math.sin(lastHeading+angleForCos+rotAngle);
+
         double xcomp = powerFinal*Math.cos(lastHeading+angleForCos);
         double ycomp = powerFinal*Math.sin(lastHeading+angleForCos);
 
-        double rotAngle = -angleForCos-angleForCos;
+
+//        xcomp = powerFinal*Math.cos(lastHeading+angleForCos+rotAngle);
+//        ycomp = powerFinal*Math.sin(lastHeading+angleForCos+rotAngle);
 
         if (tangent[3] > loc.x) {
             double xT = xcomp;
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         MyFrame frame = new MyFrame(700,700, "GuidingVecField Visual");
 
-        BezierCurveHandler bezierCurveHandler = new BezierCurveHandler(new Point(0,0),new Point(350,0), new Point(0,700),new Point(700,700));
+        BezierCurveHandler bezierCurveHandler = new BezierCurveHandler(new Point(350,0),new Point(0,0), new Point(0,0),new Point(350,700));
         GuidingVectorField gvf = new GuidingVectorField();
         Graphics2D g2d = (Graphics2D) frame.getGraphics();
         g2d.setColor(Color.RED);
@@ -16,16 +16,17 @@ public class Main {
         for (int x = 0; x < 700; x+=70) {
             for (int y = 0; y < 700; y+=70) {
                 g2d.setColor(Color.BLACK);
+                g2d.drawOval(x-5,y-5,10,10);
                 Point ssigma = gvf.getVecParallel(bezierCurveHandler, new Point(x,y));
                 g2d.drawLine(x,y, (int) ssigma.x, (int) ssigma.y);
                 g2d.setColor(Color.RED);
                 Point ssigma2 = gvf.getVec(bezierCurveHandler, new Point(x,y));
                 g2d.drawLine(x,y, (int) ssigma2.x,(int) ssigma2.y);
 
-                g2d.drawString(gvf.getClosestValueOfT(bezierCurveHandler,new Point(x,y),0.0)[0].toString(), x, y);
-                g2d.setColor(Color.RED);
-                g2d.drawString(String.valueOf(gvf.getClosestValueOfT(bezierCurveHandler,new Point(x,y),0.0)[1].intValue()),x,y-10);
-
+//                g2d.drawString(gvf.getClosestValueOfT(bezierCurveHandler,new Point(x,y),0.0)[0].toString(), x, y);
+//                g2d.setColor(Color.RED);
+//                g2d.drawString(String.valueOf(gvf.getClosestValueOfT(bezierCurveHandler,new Point(x,y),0.0)[1].intValue()),x,y-10);
+//
                 g2d.setColor(Color.blue);
                 g2d.drawLine((int) ssigma2.x, (int) ssigma2.y, (int) ssigma.x, (int) ssigma.y);
             }
